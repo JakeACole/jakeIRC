263f72d0c33f5e14bf515e6b503d9c7809985a39
Jake <Jakeacole@gmail.com>
2015-07-29 20:03:40 -0400

    Add user, join, part functionality

--- client/client.js
+++ client/client.js
@@ -1,9 +1,4 @@
-//These are meteor arrays of objects that are displayed as dummy data at the moment
-
-/*userList = [
-  {username: 'Arch_client'},
-  {username: 'Winter[Arch]'}
-];*/
+//Client
 
 channelList = [
   {channelname: '#winter-irc-test'}
@@ -16,6 +11,7 @@ if (Meteor.isClient) {
 
   //These helpers allow meteor objects to display on the GUI
   Template.ircPage.helpers({
+
     messages: function() {
       var pulledMessages = [];
         
@@ -41,45 +37,74 @@ if (Meteor.isClient) {
       var cursor = Users.find().fetch();
       var i = 0;
 
-      cursor.forEach(function(document) {
-          var time = formatTime(document.time);
+      cursor.forEach (function(document) {
 
-          pulledUsers.push({
-            users: time + document.nicks, position: i
-          });
+          /*pulledUsers.push({
+            users: document.nicks, position: i;
+          });*/
+
+          for (index = 0; index < document.nicks.length; ++index) {
+            pulledUsers.push(document.nicks[index]);
+          }
 
           i++;
       });
 
       return pulledUsers.slice();
     },
-
     channels : channelList
   });
+  
+  Template.ircPage.rendered = function () {
 
+    /*$('.msg-btn')."click"(function(e) {
+
+        $("chat-window").animate({
+          "scrollBottom": window.scrollY+300 }, 
+        1000);
+
+        return false;
+    });*/
+  }
+  
   function formatTime(milli) {
     var date =  new Date(milli);
     var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
 
     if(minutes < 10){
         minutes = '0' + minutes;
     }
 
-    return '[' + date.getHours() + ':' + minutes + '] ';
+    if(seconds < 10){
+        seconds = '0' + seconds;
+    }
+
+    return '[' + date.getHours() + ':' + minutes + ':' + seconds + '] ';
   }
 
   //These events allow the send message and enter key to send messages to the irc channel
   Template.ircPage.events({
     'click #msg-btn': function(event, template) {
-        Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
-        $('#msg-bar').val('');
+      Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
+      $('#msg-bar').val('');
     },
 
     'keypress #msg-bar': function(event, template) {
-        if(event.charCode == 13){
-            Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
-            $('#msg-bar').val('');
-        }
+      if(event.charCode == 13){
+        Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
+        $('#msg-bar').val('');
+      }
+    },
+
+    'click .logout-btn': function(event, template) {
+      e.preventDefault();
+      var result = confirm("Are you sure you want to logout?");
+
+      // If they want to logout, log them out and redirect them to the login screen
+      if(result){
+        window.close();
+      }
     }
 
   });
--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
+//Server
+
 if (Meteor.isServer) {
 
   Messages = new Mongo.Collection("messages");
   Users = new Mongo.Collection("users");
 
-  var irc = Meteor.npmRequire("irc");
-  //var irc = Meteor.require("irc");
+  //var irc = Meteor.npmRequire("irc");
+  var irc = Meteor.require("irc");
 
   //These functions allow the irc client to connect, 
-
-  var client = new irc.Client('irc.rizon.net', 'Arch_Client', {
+  var client = new irc.Client('irc.freenode.net', '[Arch]Client', {
     port: 6667,
     channels: ['#winter-irc-test'],
     localAddress: null,
     debug: false,
     showErrors: false,
-    autoRejoin: true,
+    autoRejoin: false,
     autoConnect: true,
     secure: false,
     selfSigned: false,
@@ -28,7 +29,26 @@ if (Meteor.isServer) {
     encoding: ''
   });
 
-  client.connect();
+  /*if (client.opt.floodProtection) {
+    client.activateFloodProtection();
+  }*/
+
+  if (client.opt.autoConnect === true) {
+    client.connect();
+    Messages.remove({});
+    Users.remove({});
+    Users.insert({
+      nicks: ""
+    });
+    logMessage('Server', 'Connected to #winter-irc-test');
+  }
+
+  //catches errors
+  client.addListener('error', Meteor.bindEnvironment (function(message) {
+    console.log('error: ', message);
+    /*client.disconnect('disconnecting from the irc');
+    client.connect();*/
+  }));
 
   //addListener allows for messages to be recieved
   client.addListener('message', Meteor.bindEnvironment(function (from, to, message) {
@@ -38,19 +58,23 @@ if (Meteor.isServer) {
 
   client.addListener('names#winter-irc-test', Meteor.bindEnvironment(function (nicks) {
     console.log(nicks);
-    //logUsers(nicks);
+    updateUsers(nicks);
+  }));
+
+  client.addListener('join#winter-irc-test', Meteor.bindEnvironment(function (nick) {
+    logMessage(nick, 'has joined the channel');
+  }));
+
+  client.addListener('part#winter-irc-test', Meteor.bindEnvironment(function (nick) {
+    logMessage(nick, 'has left the channel');
   }));
   
-
-  //catches errors
-  client.addListener('error', Meteor.bindEnvironment (function(message) {
-    console.log('error: ', message);
-  }));
-
-  //Allows the client to send a message 
   Meteor.methods({
+    //Allows the client to send a message 
     'sendMessage': function(message) {
       client.say('#winter-irc-test', message);
+      console.log('[Arch]Client' + ' => ' + '#winter-irc-test' + ': ' + message);
+      logMessage('[Arch]Client', message);
     }
   });
 
@@ -63,10 +87,19 @@ if (Meteor.isServer) {
     });
   }
 
-  function logUsers(nicks) {
-    Users.insert({
-        nicks: nicks,
-        time: Date.now()
+  function updateUsers(nicks) {
+    var userAra = [];
+
+    for (var property in nicks) {
+      if (nicks.hasOwnProperty(property)) {
+        userAra.push(property);
+      }
+    }
+
+    var userId = Users.findOne();
+
+    Users.update(userId._id, {$set: 
+      {nicks: userAra}
     });
   }
 }
