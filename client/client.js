0ff39dffe40435b41c0883967e6b7b7416bd5565
Jake <Jakeacole@gmail.com>
2015-07-30 00:06:09 -0400

    fix merge conflict

--- client/client.js
+++ client/client.js
@@ -1,59 +1,11 @@
-<<<<<<< HEAD
-//These are meteor arrays of objects that are displayed as dummy data at the moment
-messageList = [
-  {timestamp: '5:36', user: 'Andy', content: 'Hey, how is it going?'},
-  {timestamp: '5:37', user: 'Jake', content: 'Not bad how are you?'},
-  {timestamp: '5:37', user: 'Brandon', content: 'IRC is awesome!'},
-  {timestamp: '5:37', user: 'Walter', content: 'Yes it is'},
-  {timestamp: '5:37', user: 'Jake', content: "Can't disagree there"}
-];
-
-userList = [
-  {username: 'Andy'},
-  {username: 'Brandon'},
-  {username: 'Jake'},
-  {username: 'Walter'}
-];
-
-channelList = [
-  {channelname: '#CSS'},
-  {channelname: '#Html5'},
-  {channelname: '#JavaScript'},
-  {channelname: '#Meteor'},
-  {channelname: '#Mongo'}
-=======
 //Meteor Client JS
 
 channelList = [
   {channelname: '#winter-irc-test'}
->>>>>>> dev
 ];
 
 if (Meteor.isClient) {
 
-<<<<<<< HEAD
-  //These helpers allow meteor objects to display on the GUI
-  Template.ircPage.helpers({
-    messages: messageList,
-    users : userList,
-    channels : channelList
-  });
-
-  //These events allow the send message and enter key to send messages to the irc channel
-  Template.ircPage.events({
-    'click #msg-btn': function(event, template) {
-        Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
-        $('#msg-bar').val('');
-    },
-
-    'keypress #msg-bar': function(event, template) {
-        if(event.charCode == 13){
-            Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
-            $('#msg-bar').val('');
-        }
-    },
-
-=======
   //Used for the scrollBottom() function
   var msgCounter = 0;
 
@@ -169,6 +121,5 @@ if (Meteor.isClient) {
     }
     */
 
->>>>>>> dev
   });
 }
--- server/server.js
+++ server/server.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-if (Meteor.isServer) {
-
-  //var irc = require("irc");
-
-  //This mongo collection stores messages
-  Messages = new Mongo.Collection("messages");
-
-  //This is a channel the client can connect to
-  var ircPrm = {
-    server: 'irc.rizon.net',
-    port: 6667,
-    nick: 'Winter_EC',
-    channels: ['#dolphin-ssbm'],
-    debug: true,
-    stripColors: true
-  };
-
-  //These functions allow the irc client to connect, 
-  //they are currently being wonky with the packages I have installed
-
-  //var client = new irc(ircPrm);
-  //client.connect();
-
-  //addListener is not functioning with the current packages
-  /*client.addListener('messages', function (message) {
-    logMessage(message);
-  });*/
-
-  //Allows the client to send a message 
-  Meteor.methods({
-    'sendMessage': function(message) {
-      client.say('#dolphin-ssbm', message);
-    }
-  });
-
-  //Allows the client to insert a message into the log
-  function logMessage(message) {
-      messages.insert({
-          message: message,
-          time: Date.now(),
-      });
-  }
-}
-=======
 //Meteor Server JS
 
 //meteor deploy jakeirc.meteor.com
@@ -160,5 +115,3 @@ if (Meteor.isServer) {
     });
   }
 }
-
->>>>>>> dev
