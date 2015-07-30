dff3449698648b0ad47bf9be927798e6e76c4169
Jake <Jakeacole@gmail.com>
2015-07-29 23:54:32 -0400

    Fix userlist, cleaned up GUI

--- client/client.js
+++ client/client.js
@@ -1,4 +1,4 @@
-//Client
+//Meteor Client JS
 
 channelList = [
   {channelname: '#winter-irc-test'}
@@ -6,21 +6,22 @@ channelList = [
 
 if (Meteor.isClient) {
 
-  var q = 0;
+  //Used for the scrollBottom() function
+  var msgCounter = 0;
 
+  //Allows client to access mongo collections
   Messages = new Mongo.Collection("messages");
   Users = new Mongo.Collection("users");
 
   //These helpers allow meteor objects to display on the GUI
   Template.ircPage.helpers({
 
+    //Helper renders the messages in the chat window
     messages: function() {
       var pulledMessages = [];
         
       var cursor = Messages.find().fetch();
       var i = 0;
-
-      var windowHeight = 0;
 
       cursor.forEach(function(document) {
           var time = formatTime(document.time);
@@ -29,9 +30,10 @@ if (Meteor.isClient) {
             message: time + document.from + ': ' + document.message, position: i
           });
 
-          if (q < i) {
+          //Scrolls to the bottom if a new message has been added
+          if (msgCounter < i) {
             scrollBottom();
-            q = i;
+            msgCounter = i;
           }
 
           i++;
@@ -40,23 +42,20 @@ if (Meteor.isClient) {
       return pulledMessages.slice();
     },
 
+    //Helper renders the current user list
     users : function() {
       var pulledUsers = [];
-        
+
       var cursor = Users.find().fetch();
       var i = 0;
 
-      cursor.forEach (function(document) {
+      cursor.forEach(function(document) {
 
-          /*pulledUsers.push({
-            users: document.nicks, position: i;
-          });*/
+        for (index = 0; index < document.nicks.length; ++index) {
+          pulledUsers.push(document.nicks[index]);
+        }
 
-          for (index = 0; index < document.nicks.length; ++index) {
-            pulledUsers.push(document.nicks[index]);
-          }
-
-          i++;
+        i++;
       });
 
       return pulledUsers.slice();
@@ -64,11 +63,12 @@ if (Meteor.isClient) {
     channels : channelList
   });
 
-  //when a message is entered, scroll to the bottom
+  //When a message is entered, scroll to the bottom
   function scrollBottom() {
     $(".chat-window").animate({scrollTop:$(".chat-window")[0].scrollHeight}, $(".chat-window").height());
   }
   
+  //Formats the time displayed in the irc chat
   function formatTime(milli) {
     var date =  new Date(milli);
     var minutes = date.getMinutes();
@@ -85,20 +85,27 @@ if (Meteor.isClient) {
     return '[' + date.getHours() + ':' + minutes + ':' + seconds + '] ';
   }
 
-  //These events allow the send message and enter key to send messages to the irc channel
+  
   Template.ircPage.events({
+    //These events allow the send message and enter key to send messages to the irc channel
     'click #msg-btn': function(event, template) {
-      Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
+      Meteor.call('sendMessage', template.find('#msg-bar').value);
       $('#msg-bar').val('');
     },
 
     'keypress #msg-bar': function(event, template) {
       if(event.charCode == 13){
-        Meteor.call('sendMessage', template.find('#msg-bar').value, 'USER');
+        Meteor.call('sendMessage', template.find('#msg-bar').value);
         $('#msg-bar').val('');
       }
     },
 
+    //Allows the user to clear the messages on the screen
+    'click #clear-btn' : function (event, template) {
+      Meteor.call('clearMessages');
+    }
+
+    /*,
     'click .logout-btn': function(e) {
 
       /*e.preventDefault();
@@ -106,12 +113,13 @@ if (Meteor.isClient) {
 
       if(result){
         window.close();
-      }*/
+      }
     },
 
     'click .msg-btn': function(e) {
 
     }
+    */
 
   });
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
-//Server
+//Meteor Server JS
+
 //meteor deploy jakeirc.meteor.com
 
 if (Meteor.isServer) {
 
+  //Mongo Collections that store the users and messages
   Messages = new Mongo.Collection("messages");
   Users = new Mongo.Collection("users");
 
   //var irc = Meteor.npmRequire("irc");
   var irc = Meteor.require("irc");
 
-  //These functions allow the irc client to connect, 
+  //This initializes the IRC client 
   var client = new irc.Client('irc.freenode.net', '[Arch]Client', {
     port: 6667,
     channels: ['#winter-irc-test'],
@@ -29,57 +31,63 @@ if (Meteor.isServer) {
     messageSplit: 512,
     encoding: ''
   });
+ 
+  //This allows the irc client to connect
 
-  /*if (client.opt.floodProtection) {
-    client.activateFloodProtection();
-  }*/
+  client.connect();
 
-  //if (client.opt.autoConnect === true) {
-    client.connect();
-    Messages.remove({});
-    Users.remove({});
-    Users.insert({
-      nicks: ""
-    });
-    logMessage('Server', 'Connected to #winter-irc-test');
-  //}
+  Messages.remove({});
+  Users.remove({});
 
-  //catches errors
+  Users.insert({
+    nicks: ""
+  });
+
+  logMessage('Server', 'Connected to #winter-irc-test');
+
+  //catches errors that the client may throw
   client.addListener('error', Meteor.bindEnvironment (function(message) {
     console.log('error: ', message);
-    /*client.disconnect('disconnecting from the irc');
-    client.connect();*/
   }));
 
-  //addListener allows for messages to be recieved
+  //Listener adds messages to the collection
   client.addListener('message', Meteor.bindEnvironment(function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
     logMessage(from, message);
   }));
 
+  //Listener adds users to the collection
   client.addListener('names#winter-irc-test', Meteor.bindEnvironment(function (nicks) {
     console.log(nicks);
     updateUsers(nicks);
   }));
 
+  //Listener alerts user when a person joins the channel
   client.addListener('join#winter-irc-test', Meteor.bindEnvironment(function (nick) {
     logMessage(nick, 'has joined the channel');
   }));
 
+  //Listener alerts user when a person leaves the channel
   client.addListener('part#winter-irc-test', Meteor.bindEnvironment(function (nick) {
     logMessage(nick, 'has left the channel');
   }));
   
+  //Meteor Methods
   Meteor.methods({
     //Allows the client to send a message 
     'sendMessage': function(message) {
       client.say('#winter-irc-test', message);
       console.log('[Arch]Client' + ' => ' + '#winter-irc-test' + ': ' + message);
       logMessage('[Arch]Client', message);
+    },
+
+    //Allows the client to clear the messages
+    'clearMessages' : function() {
+      Messages.remove({});
     }
   });
 
-  //Allows the client to insert a message into the log
+  //Inserts a message into the collection
   function logMessage(from, message) {
     Messages.insert({
         from: from,
@@ -88,6 +96,7 @@ if (Meteor.isServer) {
     });
   }
 
+  //Updates the userlist 
   function updateUsers(nicks) {
     var userAra = [];
 
@@ -97,8 +106,10 @@ if (Meteor.isServer) {
       }
     }
 
+    //finds the only User object in the collection
     var userId = Users.findOne();
 
+    //Updates the User object
     Users.update(userId._id, {$set: 
       {nicks: userAra}
     });
